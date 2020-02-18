bd0cd313fe860c4126dade4608484620a4231261
JDPescosta <jamesdpescosta@gmail.com>
2020-02-17 16:52:09 -0800

    Finished first iteration of the portfolio. Landing and about section are complete.

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import shortid from 'shortid';
 import './HomePage.scss';
 import profilePic from '../../assets/images/profile-pic.png';
 import linkedinPic from '../../assets/images/linkedin-icon.png';
 import githubPic from '../../assets/images/github-icon.png';
 import cvPic from '../../assets/images/cv-icon.png';
+import downArrowPic from '../../assets/images/down-arrow-icon.png';
 import Card from '../../components/Card/Card';
 import SocialLink from '../../components/SocialLink/SocialLink';
-
 
 const socialInfo = [
   {
@@ -28,32 +28,68 @@ const socialInfo = [
   }
 ];
 
-const HomePage = () => (
-  <>
-    <div className="landing-container">
-      <img src={profilePic} alt="A cartoon avatar of James" />
+const HomePage = () => {
+  const aboutRef = useRef(null);
 
-      <Card className="heading-card">
-        <h1>Hi I'm James,</h1>
-        <h3>A software developer with experience in web and mobile.</h3>
-      </Card>
-    </div>
-    <div className="about-container">
-      <Card className="about-card">
-        <h2>WHO I AM</h2>
-        <p>this is example text</p>
-      </Card>
-      <Card className="about-card link-card">
-        {socialInfo.map(({ image, alt, link, ...props }) => (
-          <SocialLink key={shortid.generate()} imageSrc={image} altText={alt} link={link} {...props} />
-        ))}
-      </Card>
-      <Card className="about-card">
-        <h2>WHAT I DO</h2>
-        <p>this is example text</p>
-      </Card>
-    </div>
-  </>
-);
+  const scrollToRef = () => {
+    window.scrollTo({
+      left: 0,
+      top: aboutRef.current.offsetTop,
+      behavior: 'smooth'
+    });
+  };
+
+  return (
+    <>
+      <div className="landing-container">
+        <div className="landing-row">
+          <img
+            className="profile-img"
+            src={profilePic}
+            alt="A cartoon avatar of James"
+          />
+
+          <Card className="heading-card">
+            <h1>Hi I'm James,</h1>
+            <h3>A rookie developer with experience in web and games.</h3>
+          </Card>
+        </div>
+
+        <button
+          type="button"
+          className="down-button"
+          onClick={() => scrollToRef()}
+        >
+          <img src={downArrowPic} alt="down arrow" />
+        </button>
+      </div>
+      <div className="about-container" ref={aboutRef}>
+        <Card className="about-card">
+          <h2>WHO I AM</h2>
+          <p>I'm an Australian developer who recently moved to Vancouver after honeymooning with my new wife. While it's a tad bit colder here, I find myself warming up to the Canadian lifestyle.</p>
+          <br/>
+          <p>I spend most of my time coding, playing D&D and walking my dog. I'm a huge nerd!</p>
+        </Card>
+        <Card className="about-card link-card">
+          {socialInfo.map(({ image, alt, link, ...props }) => (
+            <SocialLink
+              key={shortid.generate()}
+              imageSrc={image}
+              altText={alt}
+              link={link}
+              {...props}
+            />
+          ))}
+        </Card>
+        <Card className="about-card">
+          <h2>WHAT I DO</h2>
+          <p>I originally studied a Bachelor of Games Development in Australia. After finishing my degree, I was employed as a Business Analyst where I was exposed to broader elements of the software development industry.</p>
+          <br/>
+          <p>Now that I've moved to Canada, I'm looking to get back into the development side of software. </p>
+        </Card>
+      </div>
+    </>
+  );
+};
 
 export default HomePage;
